948bef9a05c238b901afb5b511c45f1a18be6fd3
ARINDAM BHANDARI <136267807+arindam0011@users.noreply.github.com>
2024-06-12 00:54:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+	const btn= document.getElementById("btn");
+
+btn.addEventListener("click", delt);
+
+function delt(){
+	let color= document.getElementById("colorSelect");
+
+	let delcolor=document.getElementById(color.value);
+	delcolor.remove();
+	
+}
